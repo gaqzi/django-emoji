35950f2dbd8d46c8a62351a564ac33107c26fbce
Björn Andersson <ba@sanitarium.se>
2013-05-08 00:38:15 +0800

    Javascript for Emojis
    Javascript tests uses PhantomJS, originally it was running inside of Node.js
    but that wasn't working out when it came to the DOM. While PhantomJS is
    Chrome it is still Good Enough(TM) to cover most cases.

--- emoji/views.py
+++ emoji/views.py
@@ -6,7 +6,7 @@ from django.views.generic import View
 from . import Emoji
 
 
-class EmojiListView(View):
+class EmojiJSONListView(View):
     def get(self, request, *args, **kwargs):
         return HttpResponse(json.dumps(dict(Emoji)),
                             content_type='application/json')
