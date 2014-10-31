ee7b5353c039d6e1d2aeabcb084aee79e07b71f8
tschilling <schillingt@better-simple.com>
2014-10-31 10:29:26 -0400

    Update the filters to escape the characters before replacing them.
    This prevents the filters from allowing XSS attacks. This follows
    the pattern used by django's linebreaksbr filter.

--- emoji/templatetags/emoji_tags.py
+++ emoji/templatetags/emoji_tags.py
@@ -1,7 +1,8 @@
 from django import template
 from django.core.urlresolvers import reverse, NoReverseMatch
 from django.template.defaultfilters import stringfilter
-from django.utils.safestring import mark_safe
+from django.utils.safestring import mark_safe, SafeData
+from django.utils.html import escape
 
 
 from emoji import Emoji
@@ -9,21 +10,31 @@ from emoji import Emoji
 register = template.Library()
 
 
-@register.filter(name='emoji_replace', is_safe=True)
+@register.filter(name='emoji_replace', is_safe=True, needs_autoescape=True)
 @stringfilter
-def emoji_replace(value):
+def emoji_replace(value, autoescape=None):
+    autoescape = autoescape and not isinstance(value, SafeData)
+    if autoescape:
+        value = escape(value)
     return mark_safe(Emoji.replace(value))
 
 
-@register.filter(name='emoji_replace_unicode', is_safe=True)
+@register.filter(name='emoji_replace_unicode', is_safe=True, needs_autoescape=True)
 @stringfilter
-def emoji_replace_unicode(value):
+def emoji_replace_unicode(value, autoescape=None):
+    autoescape = autoescape and not isinstance(value, SafeData)
+    if autoescape:
+        value = escape(value)
     return mark_safe(Emoji.replace_unicode(value))
 
 
-@register.filter(name='emoji_replace_html_entities', is_safe=True)
+@register.filter(name='emoji_replace_html_entities',
+                 is_safe=True, needs_autoescape=True)
 @stringfilter
-def emoji_replace_html_entities(value):
+def emoji_replace_html_entities(value, autoescape=None):
+    autoescape = autoescape and not isinstance(value, SafeData)
+    if autoescape:
+        value = escape(value)
     return mark_safe(Emoji.replace_html_entities(value))
 
 
