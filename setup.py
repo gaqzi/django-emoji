cf1de931650a3a5d30cd39e621edc5557af95da8
Björn Andersson <ba@sanitarium.se>
2015-12-30 09:04:41 +0800

    Update package version to 2.1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-emoji',
-    version='2.0.0',
+    version='2.1.0',
     packages=find_packages(exclude=('test',)),
     include_package_data=True,
     license='BSD License',
