fd01a788d84289d0858f0632d42f5fd2467ac146
jrabbit <jackjrabbit@gmail.com>
2016-06-20 17:02:48 -0400

    Start getting ready for 1.10

--- emoji/urls.py
+++ emoji/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import EmojiJSONListView
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^all.json$', EmojiJSONListView.as_view(), name='list.json'),
-)
+]
